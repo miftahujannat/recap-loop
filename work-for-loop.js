3d99974eb9ffcbd5e1e0f8603ea06c27cb451afa
miftahujannat <miftahurimny@gmail.com>
2024-04-24 18:24:24 +0600

    added

--- work-for-loop.js
+++ work-for-loop.js
@@ -3,13 +3,13 @@
 
 //  }
 
-// for (var i = 100; i <= 100; i+=2){
+ for (var i = 100; i <= 1; i--){
+     console.log(i);
+
+ }
+
+// for (var i = 100; i >= 1; i-=2){
 //     console.log(i);
 
 // }
-
-for (var i = 100; i >= 1; i-=2){
-    console.log(i);
-
-}
 
---
+++ work-while-loop.js
@@ -0,0 +1,17 @@
+// var i = 0;
+// while(i < 100){
+//     i++;
+//     console.log(i);
+// }
+
+// var i = 100;
+// while(i > 1){
+//     i--;
+//     console.log(i);
+// }
+
+var i = 100;
+while(i > 1){
+    i--;
+    console.log(i);
+}
