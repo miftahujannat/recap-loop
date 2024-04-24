05a2a4aeb8b731893d5e1e5a60f2da9dd30bbfb5
miftahujannat <miftahurimny@gmail.com>
2024-04-24 17:11:41 +0600

    added

---
+++ array-loop.js
@@ -0,0 +1,12 @@
+// var Number = [45, 85, 95, 65, 75, 98 , 12, 19];
+// for(var i = 0; i < Number.length; i++){
+//     var element = Number[i];
+//     console.log(element);
+// }
+
+
+var item = ['rimi', 'sam','halua', 'jemes', 'salad'];
+for (var i = 0; i < item.length; i++){
+    var element = item[i];
+    console.log(element);
+}
