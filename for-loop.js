a8d960f83d834d538abdba68597cc81100fdab63
miftahujannat <miftahurimny@gmail.com>
2024-04-24 16:39:49 +0600

    added

--- for-loop.js
+++ for-loop.js
@@ -3,8 +3,19 @@
 //     console.log('rost den,pleace');
 //     roastgiven++;
 // }
-console.log('running a for loop');
-for (var roastgiven = 0; roastgiven < 7; roastgiven++) {
-    console.log('roast den , pleace')
-    console.log(roastgiven);
+// console.log('running a for loop');
+// for (var roastgiven = 0; roastgiven < 7; roastgiven++) {
+//     console.log('roast den , pleace')
+//     console.log(roastgiven);
+// }
+
+// for (var i = 1; i <= 10;  i++){
+//     console.log(i);
+// }
+
+// odd Number
+// i+=2
+// i = i + 2
+for (var i = 1; i <= 20;  i+=2){
+    console.log(i);
 }
