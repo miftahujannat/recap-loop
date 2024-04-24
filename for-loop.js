fccda57fb0dace69cee28d12939727718b7375c5
miftahujannat <miftahurimny@gmail.com>
2024-04-24 16:28:49 +0600

    added

---
+++ for-loop.js
@@ -0,0 +1,10 @@
+// var roastgiven = 0;
+// while(roastgiven < 7){
+//     console.log('rost den,pleace');
+//     roastgiven++;
+// }
+console.log('running a for loop');
+for (var roastgiven = 0; roastgiven < 7; roastgiven++) {
+    console.log('roast den , pleace')
+    console.log(roastgiven);
+}
