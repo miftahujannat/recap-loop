57df114c499ba2c12f72cb13e61cee12799d87fb
miftahujannat <miftahurimny@gmail.com>
2024-04-24 15:39:00 +0600

    added

--- while-loop-example.js
+++ while-loop-example.js
@@ -5,8 +5,17 @@ var number = 1;
     
 // }
 
+// while (number <= 20){
+//     console.log(number);
+//     number = number + 2;
+    
+// }
+
+
+// even number 
+var number = 0;
 while (number <= 20){
     console.log(number);
     number = number + 2;
-    
 }
+
