35d8f167ea8a0d282ea1ba5a97d09420f2af19ad
miftahujannat <miftahurimny@gmail.com>
2024-05-01 11:06:43 +0600

    added

---
+++ array.js
@@ -0,0 +1,13 @@
+var items = ['chips', 'beskut', 'berger', 'chokolate'];
+var chokolatekhabo = false;
+for (var i = 0; i < items.length; i++) {
+    if (items[i] == 'gyh') {
+        chokolatekhabo = true;
+    }
+}
+
+if (chokolatekhabo == true) {
+    console.log('I love chokolate');
+} else {
+    console.log('onno kisu valo nah');
+}
